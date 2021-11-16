77387d065420ed0bd8a8ed6fad0152a61e9ef9cb
Lukas Vidfar <lgkvidfar@gmail.com>
2021-11-16 12:43:36 +0100

    stable build, big update

--- index.js
+++ index.js
@@ -1,68 +1,10 @@
-const config = require('./utils/config')
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+const server = http.createServer(app)
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(requestLogger)
-app.use(cors())
-
-
-app.get('/', (request, response) => {
-    response.json({message: "hello world"})
-  })
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({})
-    .then(returned => {
-        response.json(returned)
-    })
-    .catch(error => {
-        console.log('error @ /api/blogs/', error)
-        response.status(500).end()
-    })
-})
-
-app.get('/api/blogs/:id', (request, response) => {
-    Blog.findById(request.params.id).then(blog => {
-        if(blog) {
-            response.json(blog)
-        } else {
-            response.status(404).send( {error: "id doesn't exist" } )
-        }
-    })
-    .then(returned => {
-        response.json(returned)
-    })
-    .catch(error => {
-        console.log('error @ /api/blogs/', error)
-        response.status(500).end()
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
