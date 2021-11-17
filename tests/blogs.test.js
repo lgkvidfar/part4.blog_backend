5f29920f6635b5968121bdc522939caf3532f3fc
Lukas Vidfar <lgkvidfar@gmail.com>
2021-11-17 14:59:48 +0100

    make it better

---
+++ tests/blogs.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+// test('blogs are returned as json', async () => {
+//   await api
+//     .get('/blogs')
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
+// })
+
+test('there are two blogs', async () => {
+    const response = await api.get('/blogs')
+  
+    expect(response.body).toHaveLength(2)
+  })
+  
+  test('the third blog is written by BigBags', async () => {
+    const response = await api.get('/blogs')
+    console.log(response)
+  
+    expect(response.body).toBe('BigBags')
+  })
+
+  afterAll(() => {
+    mongoose.connection.close()
+  })
